2461791720a4cdf3309b7ac7f15b932ade319362
David Maack <maack@men-at-work.de>
2013-01-17 16:22:47 +0100

    Added runonce to convert existing selectModule-Installation to support forms

---
+++ system/modules/selectModule/config/runonce.php
@@ -0,0 +1,62 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  MEN AT WORK 2013
+ * @package    selectModule
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+
+class SelectModuleRunonce extends Controller
+{
+	
+	public function run()
+	{
+		$this->import('Database');
+		
+        $objModules = $this->Database->prepare('SELECT id, sm_wizard FROM tl_module WHERE type = \'selectmodule\'')->executeUncached();
+        
+        while($row = $objModules->fetchAssoc())
+        {
+            $tmp = deserialize($row['sm_wizard'], true);
+            
+            foreach ($tmp as $k => $v)
+            {
+                if (is_numeric($v['module']))
+                {
+                    $tmp[$k]['module'] = $v['module'].'-module';
+                }
+            }
+            
+            $this->Database->prepare('UPDATE tl_module SET sm_wizard = ? WHERE id = ?')->executeUncached(serialize($tmp), $row['id']);
+        }
+		
+        exit();
+	}
+}
+
+
+$objSelectModuleRunonce = new SelectModuleRunonce();
+$objSelectModuleRunonce->run();
+
